691a8f72b8874a1883b1a839264a1f0ae55c76f3
danielmilano1 <121773300+danielmilano1@users.noreply.github.com>
2023-06-25 15:56:31 -0600

    added time script

---
+++ time.py
@@ -0,0 +1,8 @@
+import time
+
+# Simulate some time-consuming operation
+def time_consuming_operation():
+    time.sleep(1)  # Sleep for 1 second
+
+# Perform the time-consuming operation
+time_consuming_operation()
